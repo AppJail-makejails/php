9d3108e26b924866681588541448eed015187fb1
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2024-12-14 13:06:20 -0400

    Add scripts to update files more easily, Add `php_ajspec` argument

---
+++ update/update.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+BASEDIR=`dirname -- "$0"` || exit $?
+BASEDIR=`realpath -- "${BASEDIR}"` || exit $?
+
+. "${BASEDIR}/update.conf"
+
+set -xe
+set -o pipefail
+
+cat -- "${BASEDIR}/Makejail.template" |\
+    sed -Ee "s/%%TAG1%%/${TAG1}/g" > "${BASEDIR}/../Makejail"
+
+cat -- "${BASEDIR}/README.md.template" |\
+    sed -E \
+        -e "s/%%TAG1%%/${TAG1}/g" \
+        -e "s/%%TAG2%%/${TAG2}/g" \
+        -e "s/%%PHP1%%/${PHP1}/g" \
+        -e "s/%%PHP2%%/${PHP2}/g" \
+        -e "s/%%PHP3%%/${PHP3}/g" \
+        -e "s/%%PHP4%%/${PHP4}/g" > "${BASEDIR}/../README.md"
+
+cat -- "${BASEDIR}/build.makejail.template" |\
+    sed -E \
+        -e "s/%%PHP1%%/${PHP1}/g" \
+        -e "s/%%PHP2%%/${PHP2}/g" \
+        -e "s/%%PHP3%%/${PHP3}/g" \
+        -e "s/%%PHP4%%/${PHP4}/g" > "${BASEDIR}/../build.makejail"
